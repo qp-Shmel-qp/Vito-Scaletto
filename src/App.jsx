f0d8064eb3663b8efad03fe910f9b749a32a305e
Chmil Daniil <daniilcmil12@gmail.com>
2024-12-21 16:38:58 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { useState } from 'react';
+
+
+export default function Board() {
+
+  return (
+    <>
+      <div className="board-row">
+      hello world
+      </div>
+    </>
+  );
+}
+
